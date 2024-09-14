1f603211c238aab065d5e2ecedc6ac2f21e5ae85
Leandro <leandrosarkis1602@yahoo.com>
2024-09-14 11:28:51 -0300

    final test

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Mario x Bianca')
+    print_hi('Leandro x Fabiana')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
