417a2f50b7bec3011cbebf591725785a8da2ac40
Leandro <leandrosarkis1602@yahoo.com>
2024-09-26 14:49:33 -0300

    Colocando método para salvar dataframe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from tabulate import tabulate
+
 pd.options.display.width = 0
 pd.set_option('display.max_rows', None)
 
@@ -34,4 +32,7 @@ df = df.drop_duplicates()
 df = df[df.Year != 2017]
 df = df[df.Year != 2020]
 
+# Salvar o dataframa tratado
+csv_file_path = "clean-data/clean-dataframe.csv"
+df.to_csv(csv_file_path, index=False)
 
